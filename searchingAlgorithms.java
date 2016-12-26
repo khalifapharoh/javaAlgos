92b5a0bd400685daec7df896f5bc6a8447f8ade8
khalifapharoh <mkhalifa.fci@outlook.com>
2016-12-26 19:21:13 +0200

    Update searchingAlgorithms.java

--- searchingAlgorithms.java
+++ searchingAlgorithms.java
@@ -17,6 +17,28 @@ public class searchingAlgorithms {
         }
         return false;
     }
+      public static boolean binarySearch(int[] numbers, int value)
+    {
+        //The Array must be sorted to use the binary search Algorithm so I'am using my insertion sort algorithm to sort the array.
+        //You can find the insertion sort algorithm source code in the sortingAlgortihms.java file in the same repository.
+        InsertionSort(numbers);
+        int low = 0 ,high =numbers.length;
+        for (int i = 0; i < high; i++) {
+            if(numbers[(low+high) / 2] == value)
+                return true;
+            else if(numbers[(low+high) / 2] > value)
+            {
+                high = (high+ low)/2;
+                continue;
+            }
+              else if(numbers[(low+high) / 2] < value)
+            {
+                low  = (high+ low)/2;
+                continue;
+            }
+        }
+        return false;
+    }
     
 }
 
